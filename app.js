e33c56ccf745d5b2ebcfb911cdd1681526aefa0c
Gynidark <gynidark@hotmail.com>
2016-07-21 22:31:13 +0200

    :heart:

--- app.js
+++ app.js
@@ -5,18 +5,22 @@ var io = require('socket.io')(server);
 server.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendfile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-    console.log('Utilisateur connecté!');
-
-    socket.on('chatMessage', (msg) => {
-        console.log('message: ' + msg);
+io.on('connection', socket => {
+    // console.log('Utilisateur connecté');
+    
+    socket.on('chatMessage', msg => {
+        //console.log('message: ' + msg);
         io.emit('chatMessage', msg);
     });
 
     socket.on('disconnect', _ => {
-        io.emit('Utilisateur déconnecté!');
+        io.emit('Utilisateur déconnecté');
+    });
+
+    socket.on('typing', data => {
+        io.emit('typing', data);
     });
 });
