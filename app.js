db6efd6888fb504afca6864df637ea4321ef49b8
Gynidark <gynidark@gmail.com>
2017-06-23 12:36:53 +0200

    :sparkles: Add style CSS for stylish form connect

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.get('/', (req, res) => {
 })
 
 app.use(express.static(__dirname + '/js'))
+app.use(express.static(__dirname + '/css'))
 
 io.on('connection', socket => {    
     console.log('Utilisateur connecté')
