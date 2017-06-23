28828ee475975f1986ebdcdb203656c0312f20fc
Gynidark <gynidark@gmail.com>
2017-06-23 10:30:23 +0200

    :five: Remove ';'

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-var express = require('express');
-var app = express();
-var port = 3000;
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var id = Math.round((Math.random() * 1000000));
+var express = require('express')
+var app = express()
+var port = 3000
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
+var id = Math.round((Math.random() * 1000000))
 
 server.listen(port, function(){
-    console.log('Wesh::port => '+ port);
-});
+    console.log('Wesh::port => '+ port)
+})
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+    res.sendFile(__dirname + '/index.html')
+})
 
-app.use(express.static(__dirname + '/js'));
+app.use(express.static(__dirname + '/js'))
 
-// connexion socket
+
 io.on('connection', socket => {    
-    console.log('Utilisateur connecté');
+    console.log('Utilisateur connecté')
 
     socket.on('chatMessage', msg => {
-        console.log('message: ' + msg);
-        io.emit('chatMessage', msg);
-    });
+        console.log('message: ' + msg)
+        io.emit('chatMessage', msg)
+    })
 
     socket.on('disconnect', _ => {
-        console.log('Utilisateur déconnecté');        
-    });
-});
+        console.log('Utilisateur déconnecté')
+    })
+})
