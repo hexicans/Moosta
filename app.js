6f8d9eb381c7c6716cecf3d95a6bcb430ce6306a
Gynidark <gynidark@gmail.com>
2017-06-26 10:04:14 +0200

    :sparkles: Refactoring code

--- app.js
+++ app.js
@@ -5,26 +5,20 @@ var server = require('http').Server(app)
 var io = require('socket.io')(server)
 var id = Math.round((Math.random() * 1000000))
 
-server.listen(port, function(){
-    console.log('Serveur bien démarré avec le port : '+ port)
-})
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
-
+// Path assets
 app.use(express.static(__dirname + '/js'))
 app.use(express.static(__dirname + '/css'))
 
+// Routes
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+
+// Socket.IO
 io.on('connection', socket => {    
     console.log('Utilisateur connecté')
 
-    socket.on('chatMessage', msg => {
-        console.log('message: ' + msg)
-        io.emit('chatMessage', msg)
-    })
-
-    socket.on('disconnect', () => {
-        console.log('Utilisateur déconnecté')
-    })
+    socket.on('chatMessage', msg => io.emit('chatMessage', msg))
+    socket.on('disconnect', () => console.log('Utilisateur déconnecté'))
 })
+
+// Server
+server.listen(port, () => console.log('Serveur bien démarré avec le port : ' + port))
